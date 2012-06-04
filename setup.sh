36ca7a503224741a04e23ceea7c929c9bf244959
Langtian Lang <llang@yammer-inc.com>
2012-06-04 14:44:02 -0700

    add setup script

---
+++ setup.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#############################
+# setup.sh
+# Setup the given environment with all dotfiles required
+#
+# This should be run after the whole dotfiles repo is cloned into ~/dotfiles
+#############################
+
+dir=~/dotfiles                    "dotfile dir"
+olddir=~/dotfiles/old             "dotfile dir for backup previous settings"
+files="vimrc vim zshrc oh-my-zsh"     "files to symlink, should always start with ."
+
+# create dotfiles_old in homedir
+echo "Creating $olddir for backup of any existing dotfiles in ~"
+mkdir -p $olddir
+echo "...done"
+
+# change to the dotfiles directory
+echo "Changing to the $dir directory"
+cd $dir
+echo "...done"
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for file in $files; do
+    echo "Moving any existing dotfiles from ~ to $olddir"
+    mv ~/.$file $olddir
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+done
+
+function install_zsh {
+# Test to see if zshell is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    # Set the default shell to zsh if it isn't currently set to zsh
+    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+        chsh -s $(which zsh)
+    fi
+else
+    # If zsh isn't installed, get the platform of the current machine
+    platform=$(uname);
+    # If the platform is Linux, try an apt-get to install zsh and then recurse
+    if [[ $platform == 'Linux' ]]; then
+        sudo apt-get install zsh
+        install_zsh
+    # If the platform is OS X, tell the user to install zsh :)
+    elif [[ $platform == 'Darwin' ]]; then
+        echo "Please install zsh, then re-run this script!"
+        exit
+    fi
+fi
+}
+
+install_zsh
